e2a3fda7f2143e3ae974497f33915176714240df
TestEngineerU <maryna_romanets@aol.com>
2022-04-05 20:37:46 -0500

    add test examples for /test endpoint

--- src/test/app.test.js
+++ src/test/app.test.js
@@ -2,9 +2,27 @@ const app = require(".././app.js")
 const supertest = require("supertest")
 const request = supertest(app)
 
+const expectedLength = 25;
+const expectedObj = {id : 23};
+
 describe("/test endpoint", () => {
-    it("should return a response", async () => {
+    it("should return status code 200", async () => {
         const response = await request.get("/test")
         expect(response.status).toBe(200);
+    })
+
+    it("should return expected body length", async () => {
+        const response = await request.get("/test")
+        expect(response.body).toHaveLength(expectedLength);
+        expect(response.body.length).toBeGreaterThan(0);
+    })
+
+    it("should return expected object in body", async () => {
+        const response = await request.get("/test")
+        expect(response.body).toEqual(
+            expect.arrayContaining([
+                expect.objectContaining(expectedObj)
+            ])
+        );
     })
 })
