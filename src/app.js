97c9ba0cb3d752134558da3aedc39a51f84e8c38
TestEngineerU <maryna_romanets@aol.com>
2022-04-05 20:08:12 -0500

    add mock data

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+const data = require('./data/mock.json')
+
 const express = require("express");
 const app = express();
 
 app.get("/test", (_req, res) =>  {
-  res.status(200).send("Hello world")
+  res.status(200).json(data);
 })
 module.exports = app;
--- src/test/app.test.js
+++ src/test/app.test.js
@@ -5,7 +5,6 @@ const request = supertest(app)
 describe("/test endpoint", () => {
     it("should return a response", async () => {
         const response = await request.get("/test")
-        expect(response.status).toBe(200)
-        expect(response.text).toBe("Hello world");
+        expect(response.status).toBe(200);
     })
 })
